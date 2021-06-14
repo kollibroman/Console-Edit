2c094e110be04950d47e7dfd575b8b249db94520
kollibroman <filipkwiek888@gmail.com>
2021-06-14 18:49:45 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+
+
+namespace txtEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           WriteLine("Welcome to the shitty txt editor!");
+
+            
+        }
+    }
+}
